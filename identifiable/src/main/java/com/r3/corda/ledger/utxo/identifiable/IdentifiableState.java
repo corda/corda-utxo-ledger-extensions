e546cf780528a38625dc2b76a1295380f4c74488
Matthew Layton <matthew.layton@live.co.uk>
2023-03-24 19:05:38 +0000

    Reverted `IdentifiableState`.

--- identifiable/src/main/java/com/r3/corda/ledger/utxo/identifiable/IdentifiableState.java
+++ identifiable/src/main/java/com/r3/corda/ledger/utxo/identifiable/IdentifiableState.java
@@ -1,5 +1,6 @@
 package com.r3.corda.ledger.utxo.identifiable;
 
+import com.r3.corda.ledger.utxo.base.VisibleState;
 import net.corda.v5.ledger.utxo.BelongsToContract;
 import net.corda.v5.ledger.utxo.ContractState;
 import net.corda.v5.ledger.utxo.StateRef;
@@ -9,7 +10,7 @@ import org.jetbrains.annotations.Nullable;
  * Defines a mechanism for implementing uniquely identifiable states.
  */
 @BelongsToContract(IdentifiableContract.class)
-public interface IdentifiableState extends ContractState {
+public interface IdentifiableState extends VisibleState {
 
     /**
      * Gets the unique ID of the current {@link IdentifiableState}, or null if it's the first state.
