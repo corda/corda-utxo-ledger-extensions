6b6c69ea7a26fb254812d2c3017c6d88ddb777df
Matthew Layton <9935122+MrMatthewLayton@users.noreply.github.com>
2023-03-27 16:58:58 +0100

    CORE-12031 : Refactor IdentifiableState to extend ContractState instead of VisibleState
    Refactored `IdentifiableState` to extend `ContractState` instead of `VisibleState`, as this should be an opt-in for CorDapp developers.

--- identifiable/src/main/java/com/r3/corda/ledger/utxo/identifiable/IdentifiableState.java
+++ identifiable/src/main/java/com/r3/corda/ledger/utxo/identifiable/IdentifiableState.java
@@ -1,6 +1,5 @@
 package com.r3.corda.ledger.utxo.identifiable;
 
-import com.r3.corda.ledger.utxo.base.VisibleState;
 import net.corda.v5.ledger.utxo.BelongsToContract;
 import net.corda.v5.ledger.utxo.ContractState;
 import net.corda.v5.ledger.utxo.StateRef;
@@ -10,7 +9,7 @@ import org.jetbrains.annotations.Nullable;
  * Defines a mechanism for implementing uniquely identifiable states.
  */
 @BelongsToContract(IdentifiableContract.class)
-public interface IdentifiableState extends VisibleState {
+public interface IdentifiableState extends ContractState {
 
     /**
      * Gets the unique ID of the current {@link IdentifiableState}, or null if it's the first state.
