fa7ad1bc2fa4290f39caa4d5a052e4eecae85d49
Matthew Layton <matthew.layton@live.co.uk>
2023-04-06 09:21:37 +0100

    Fixed a bug in `FungibleConstraints.verifyDelete`, where it was possible to create fungible states without burning an equivalent fungible state.

--- fungible/src/main/java/com/r3/corda/ledger/utxo/fungible/FungibleConstraints.java
+++ fungible/src/main/java/com/r3/corda/ledger/utxo/fungible/FungibleConstraints.java
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.NotNull;
 import java.math.BigInteger;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Represents verification constraints for creating, updating and deleting {@link FungibleState} instances.
@@ -39,6 +40,9 @@ public final class FungibleConstraints {
 
     final static String CONTRACT_RULE_DELETE_SUM =
             "On fungible state(s) deleting, the sum of the unscaled values of the consumed states must be greater than the sum of the unscaled values of the created states.";
+
+    final static String CONTRACT_RULE_DELETE_GROUP_SUM =
+            "On fungible state(s) deleting, the sum of consumed states that are fungible with each other must be greater than the sum of the created states that are fungible with each other.";
 
     /**
      * Prevents instances of {@link FungibleConstraints} from being created.
@@ -83,7 +87,7 @@ public final class FungibleConstraints {
      * @param transaction The transaction to verify.
      * @throws RuntimeException if the specified transaction fails verification.
      */
-    @SuppressWarnings({"rawtypes", "unchecked"})
+    @SuppressWarnings("rawtypes")
     public static void verifyUpdate(@NotNull final UtxoLedgerTransaction transaction) {
         final List<FungibleState> inputs = transaction.getInputStates(FungibleState.class);
         final List<FungibleState> outputs = transaction.getOutputStates(FungibleState.class);
@@ -96,21 +100,16 @@ public final class FungibleConstraints {
         for (final FungibleState input : inputs) {
 
             // 1. We need to obtain all the inputs that are fungible with the current input.
-            final List<FungibleState> fungibleInputs = inputs
-                    .stream()
-                    .filter(it -> input.isFungibleWith(it))
-                    .collect(Collectors.toList());
+            final List<FungibleState> fungibleInputs = filterByFungibleWith(inputs, input);
 
             // 2. We need to obtain all the outputs that are fungible with the current input.
-            final List<FungibleState> fungibleOutputs = outputs
-                    .stream()
-                    .filter(it -> input.isFungibleWith(it))
-                    .collect(Collectors.toList());
+            final List<FungibleState> fungibleOutputs = filterByFungibleWith(outputs, input);
 
             // 3. Generate the sum of all fungible inputs and outputs.
             final BigInteger inputSum = FungibleUtils.sum(fungibleInputs);
             final BigInteger outputSum = FungibleUtils.sum(fungibleOutputs);
 
+            // 4. Check that the sum of each input group is equal to the sum of each output group.
             Check.isEqual(inputSum, outputSum, CONTRACT_RULE_UPDATE_GROUP_SUM);
         }
     }
@@ -121,6 +120,7 @@ public final class FungibleConstraints {
      * <ol>
      *     <li>On fungible state(s) deleting, at least one fungible state input must be consumed.</li>
      *     <li>On fungible state(s) deleting, the sum of the unscaled values of the consumed states must be greater than the sum of the unscaled values of the created states.</li>
+     *     <li>On fungible state(s) deleting, the sum of consumed states that are fungible with each other must be greater than the sum of the created states that are fungible with each other.</li>
      * </ol>
      *
      * @param transaction The transaction to verify.
@@ -133,5 +133,37 @@ public final class FungibleConstraints {
 
         Check.isNotEmpty(inputs, CONTRACT_RULE_DELETE_INPUTS);
         Check.isGreaterThan(FungibleUtils.sum(inputs), FungibleUtils.sum(outputs), CONTRACT_RULE_DELETE_SUM);
+
+        // We have to check all inputs and outputs, because we might create an extra output for which there is no input.
+        final List<FungibleState> states = Stream.concat(inputs.stream(), outputs.stream()).collect(Collectors.toList());
+
+        for (final FungibleState state : states) {
+
+            // 1. We need to obtain all the inputs that are fungible with the current state.
+            final List<FungibleState> fungibleInputs = filterByFungibleWith(inputs, state);
+
+            // 2. We need to obtain all the outputs that are fungible with the current state.
+            final List<FungibleState> fungibleOutputs = filterByFungibleWith(outputs, state);
+
+            // 3. Generate the sum of all fungible inputs and outputs.
+            final BigInteger inputSum = FungibleUtils.sum(fungibleInputs);
+            final BigInteger outputSum = FungibleUtils.sum(fungibleOutputs);
+
+            // 4. Check that the sum of each input group is greater than the sum of each output group.
+            Check.isGreaterThan(inputSum, outputSum, CONTRACT_RULE_DELETE_GROUP_SUM);
+        }
+    }
+
+    /**
+     * Gets a {@link List} of {@link FungibleState} that are fungible with the specified {@link FungibleState}.
+     *
+     * @param states The {@link FungibleState} states to filter that are fungible with the specified {@link FungibleState}.
+     * @param state  The {@link FungibleState} state to determine is fungible with any of the specified {@link FungibleState} states.
+     * @return Returns a {@link List} of {@link FungibleState} that are fungible with the specified {@link FungibleState}.
+     */
+    @NotNull
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    private static List<FungibleState> filterByFungibleWith(@NotNull final List<FungibleState> states, @NotNull final FungibleState state) {
+        return states.stream().filter(it -> state.isFungibleWith(it)).collect(Collectors.toList());
     }
 }
